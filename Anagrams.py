c8c5aa32dd7fa6611bf9e19f6766fc8a999e3a88
Clara (Zoe) Kim <ckim@rollins.edu>
2017-02-23 04:21:41 +0000

    Added comments

--- Anagrams.py
+++ Anagrams.py
@@ -6,16 +6,22 @@ f= open('words.txt')
 d = {}
 
 for word in f:
+    
+    #Takes all words in file and sorts them in alphabetical order
     word = word.strip()
     word_list = [] #value for dict
     sorted_word = tuple(sorted(word)) #key for dict
     
+    #Checks if sorted word is already a key in dict
+    #If so, adds original word to list at key's value
+    #If not, creates a new key
     if sorted_word not in d:
         word_list.append(word)
         d[sorted_word] = word_list
     else:
         d[sorted_word].append(word)
 
+    #Checks for all lists in dict that has at least a pair of words
 for list in d:
     if len(d[list]) > 1:
         print d[list]
